1acb26739940aa743ec8e2cf2566f70c9ff385fd
Dennis <dennisszhou@gmail.com>
2016-11-30 19:36:26 -0600

    improvements to partner grading for cs_537

--- grade_partners.pl
+++ grade_partners.pl
@@ -6,9 +6,15 @@ use File::Find;
 
 sub max ($$) { $_[$_[0] < $_[1]] }
 
+my $handin_directory = "/u/c/s/cs537-1/handin/";
+
 my $partners = {};
 my $grades = {};
+my $correct_file_loc = {};
 my @student_list = ();
+
+# deductions
+my $deduc_wrong_file_loc = 3;
 
 # limit the depth for find traversal
 sub pp_depth {
@@ -27,9 +33,11 @@ sub find_partners {
   my $full_path = $File::Find::name;
 
   # find partner.login file in the project directory
-  if (-f and $File::Find::name =~ ".*/([^/]+)/$ARGV[0]/partner.login") {
+  if (-f and $File::Find::name =~ ".*/([^/]+)/$ARGV[0](/.*)?/partner.login\$") {
     my ($partner1, $partner2) = ("", "");
     $partner1 = $1;
+
+    $correct_file_loc->{$partner1} = 1 if (not defined $2);
 
     open my $file, '<', $full_path; 
     $partner2 = <$file>; 
@@ -58,18 +66,21 @@ sub pp_students {
 sub find_students {
   # depth == 6 is the depth of /u/c/s/cs537-1/handin/<section>
   my $depth = $File::Find::dir =~ tr[/][];
-  push @student_list, $_ if -d and $depth == 6;
+  if (-d and $depth == 6) {
+    push @student_list, $_;
+    $grades->{$_} = 0;
+  }
 }
-
-find({
-      preprocess  => \&pp_partners,
-      wanted      => \&find_partners,
-}, "/u/c/s/cs537-1/handin/");
 
 find({
       preprocess  => \&pp_students,
       wanted      => \&find_students,
-}, "/u/c/s/cs537-1/handin/");
+}, $handin_directory);
+
+find({
+      preprocess  => \&pp_partners,
+      wanted      => \&find_partners,
+}, $handin_directory);
 
 @student_list = sort @student_list;
 
@@ -89,8 +100,10 @@ foreach my $key (sort keys %{$partners}) {
   next unless (exists $grades->{$key} and exists $grades->{$partner});
 
   my $grade = max($grades->{$key}, $grades->{$partner});
-  $grades->{$key} = $grade;
-  $grades->{$partner} = $grade;
+  $grades->{$key} = (exists $correct_file_loc->{$key})
+      ? $grade : $grade - $deduc_wrong_file_loc;
+  $grades->{$partner} = (exists $correct_file_loc->{$partner})
+      ? $grade : $grade - $deduc_wrong_file_loc;
 }
 
 # print out scores
@@ -99,5 +112,6 @@ foreach my $student (@student_list) {
      | cut -d ":" -f2};
   chomp $roster;
   $roster =~ s/[\s]+/ /g;
-  print $student . " " . $grades->{$student} . " " . $roster . "\n";
+  my $grade = ($grades->{$student} > 0) ? $grades->{$student} : 0;
+  print $student . " " . $grade . " " . $roster . "\n";
 }
