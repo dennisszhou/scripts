854ddb6293733f31df59f2f2818a839ee512441b
Dennis Zhou <dennisszhou@gmail.com>
2018-09-13 13:28:20 -0700

    let's update this :/

--- qemu.sh
+++ qemu.sh
@@ -3,15 +3,16 @@
 REPO=$1
 shift;
 
-exec qemu-kvm -m 8192 -cpu host "$@" \
+exec qemu-kvm -m 8192 -cpu host "$@" -smp 2 \
 	--nographic -s -enable-kvm -usb \
 	-kernel /home/dennisz/local/$REPO/arch/x86/boot/bzImage \
 	-device e1000,netdev=net0,romfile=/home/dennisz/local/qemu-2.11.0/pc-bios/efi-e1000.rom \
 	-netdev user,id=net0,ipv6-net=fec0::/64,ipv6-host=fec0::2,ipv6-dns=fec0::3,hostfwd=tcp::5555-:22 \
-	-drive file=/home/dennisz/local/fs-img/test.img,id=disk,if=none,cache=none,discard=off
-	-device ahci,id=ahci
+	-drive file=/home/dennisz/local/fs-img/test.img,id=disk,if=none,cache=none,discard=off \
+	-device ahci,id=ahci \
 	-device ide-drive,drive=disk,bus=ahci.0 \
-	-append "console=ttyS0 earlyprintk=ttyS0 ignore_loglevel root=/dev/sda1"
+	-append "console=ttyS0 earlyprintk=ttyS0 ignore_loglevel root=/dev/sda1 \
+	cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1"
 
 # exec qemu-kvm -m 8G "$@" \
 	#   	-drive file=/home/dennisz/local/fs-img/test.img,id=disk,if=none,cache=none,discard=off \
